394dcba83b50d1bf31233f39689d4a145d5d33c8
Rojo Valisoa <rojo.valisoa.andrianantenaina@esti.mg>
2019-09-29 17:06:55 +0300

    ssdfsdfsdf

---
+++ transFacial.py
@@ -0,0 +1,19 @@
+import os
+from PIL import Image
+
+BASE_DIR = os.path.dirname(os.path.abspath(__file__))
+image_dir =  os.path.join(BASE_DIR, "images")
+
+y_labels = []
+x_train = []
+
+for root, dirs, files in os.walk(image_dir):
+    for file in files:
+            path = os.path.join(root, file)
+            label = os.path.basename(root).replace(" ","-").lower()
+            print(label,path)
+            # y_labels.append(label)
+            # x_train.append(path)
+            pil_minage  = Image.open(path).convert("L")
+            image_array = np.array
+
