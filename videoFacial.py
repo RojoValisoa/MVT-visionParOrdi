3b2028808d400e24639f03c84465b2bf9a4fede6
Rojo Valisoa <rojo.valisoa.andrianantenaina@esti.mg>
2019-10-01 14:42:59 +0300

    dataset généré -trainner.yml

--- videoFacial.py
+++ videoFacial.py
@@ -4,6 +4,8 @@ import cv2
 #detection de face
 
 face_cascade = cv2.CascadeClassifier("tools/data/haarcascade_frontalface_alt2.xml")
+recognizer = cv2.face.LBPHFaceRecognizer_create()
+recognizer.read("trainner.yml")
 cap = cv2.VideoCapture(0)
 while(True):
 
@@ -16,6 +18,12 @@ while(True):
         print(x,y,w,h)
         roi_gray = gray[y:y+h, x:x+w]
         roi_color = frame[y:y+h, x:x+w]
+        
+        id_, conf = recognizer.predict(roi_gray)
+        if conf>= 45 and conf <= 85:
+            print(id_)
+
+
         img_item = "myface.jpg"
         cv2.imwrite(img_item, roi_gray)
 
